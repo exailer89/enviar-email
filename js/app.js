a4cc6045d11a6262ef3dbe2bb302c8f97f52e0ca
Hugo Barrera <exailer89@gmail.com>
2023-10-01 21:26:19 -0600

    Añadiendo la alerta en el HTML

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
     * Eventos utilizados:
         * blur: Este evento se dispara cuando sales de un input.
-        * input: Este evento se dispara cuando estas escribiendo en el input (se presta más para validacion en tiempo real).
+        * input: Este evento se dispara cuando estas escribiendo en el input (se presta más para validación en tiempo real).
     * Acceder a Propiedades del elemento al que le agregamos evento
         inputEmail.addEventListener('blur', function(e) {
             console.log(e); // Podemos revisar las propiedades del evento.
@@ -12,6 +12,7 @@
     * trim(): Elimina los espacios vacios (al inicio y final) que el usuario pudo haber agregado en el formulario.
     * createElement(): Generamos un nuevo elemento HTML.
     * textContent: Permite agregar texto a un elemento.
+    * En la linea "inputEmail.addEventListener('blur', validar);" validar es una función, sin embargo, si le agregamos los paréntesis() la función se ejecuta inmediatamente el HTML esté listo pero, si no colocamos los paréntesis() la función será ejecutada únicamente cuando sea solicitada de acuerdo a nuestra lógica de programación.
 */
 
 document.addEventListener('DOMContentLoaded', function() { // Se ejecuta una vez todo el código HTML haya sido descargado
@@ -20,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function() { // Se ejecuta una vez
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
     
     // Asignar Eventos
     inputEmail.addEventListener('blur', validar);
@@ -28,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function() { // Se ejecuta una vez
 
     // Función para validar el formulario
     function validar(e) {
-        if (e.target.value.trim() === '') {
+        if (e.target.value.trim() === '') { // Colocar trim() nos ayuda a identificar que no exista espacios vacíos ya que si no lo agregamos, dichos espacios serán considerados como caracteres y nuestra validación no funcionará como esperamos.
             mostrarAlerta();
         } else {
             console.log('NO esta Vacio');
@@ -40,6 +42,9 @@ document.addEventListener('DOMContentLoaded', function() { // Se ejecuta una vez
         // Generar Alerta en HTML
         const error = document.createElement('P');
         error.textContent = 'Hubo un error...';
-        console.log(error);
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+        
+        // Inyectar el error al formulario
+        formulario.appendChild(error); // Seleccionamos el formulario y creamos un elemento de error, la diferencia con innerHTML es que este segundo reemplaza todo HTML.
     }
 });
